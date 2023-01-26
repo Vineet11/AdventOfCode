aa86f8cd546dbc5d27369c5a3f5c43c8af49c12c
Vineet <vineetmgh@gmail.com>
2023-01-27 02:26:43 +0530

    [2022_day17]: part 2 solved

--- 2022/day17.py
+++ 2022/day17.py
@@ -11,7 +11,6 @@ Get stone pattern based on the sequence
 '''
 NUM_STONE_PATTERNS = 5
 def get_stone_pattern(seq_id):
-    seq_id = seq_id % NUM_STONE_PATTERNS
     # assuming that stone always starts 2 steps away from left, in a cave of length 7
     stone_pattern={}
     if seq_id==0:
@@ -90,6 +89,13 @@ def detect_collision(cave, stone_pattern, height_from_ceiling):
     no_collission = np.sum((cave[start_idx:end_idx] & stone_pattern)!=0)==0
     return no_collission
 
+# Detects if the last line of the cave is like plane ground to the stone. This will 
+def detect_plane_ground(cave_last_line, seq_id = 0):
+    # Current function is hard coded to work with seq_id 0
+    possible_stone_states = np.array([15, 30, 60, 120], dtype="uint16")
+    is_plane_ground = np.sum((possible_stone_states & cave_last_line)==0)==0
+    return is_plane_ground
+
 def update_cave(cave, stone_pattern, height_from_ceiling):
     start_idx = -(height_from_ceiling+stone_pattern.size)
     end_idx = None if height_from_ceiling==0 else -height_from_ceiling
@@ -127,16 +133,30 @@ def simulate_stone_fall(cave, stone_pattern, current_jet_stream, height_from_cei
         cave = update_cave(cave, stone_pattern["pattern"], height_from_ceiling)
         return cave, stone_pattern, True
 
-def simulate_cave(input_jet_stream, num_stones):
+def simulate_cave(input_jet_stream, num_stones, detect_repetition=False, time_offset=0):
     ## Start with an empty cave. Append the rows per the stone size
     max_jet_stream_len = len(input_jet_stream)
     cave = np.zeros(0, dtype="uint16")
     visualize_pattern(cave)
-    time_step = 0
+    time_step = time_offset
     stack_height = 0
+    if detect_repetition:
+        detect_repetition_time_list = []
+        detect_repetition_height_list = []
+        detect_repetition_stone_list = []
     for i in range(num_stones):
         height_from_ceiling = 0
-        stone_pattern = get_stone_pattern(i)
+        seq_id = i % NUM_STONE_PATTERNS
+        stone_pattern = get_stone_pattern(seq_id)
+        last_line_before_fall = 0
+        stack_height_before_fall = 0
+        if detect_repetition:
+            is_plane_ground = False
+            stack_height_before_fall = stack_height
+            if int(cave.size) != 0:
+                last_line_before_fall = cave[stack_height-1 if stack_height != 0 else 0]
+            if int(cave.size) != 0 and seq_id == 0 :
+                is_plane_ground = detect_plane_ground(last_line_before_fall)
         if DEBUG:
             print("shape of the falling stone")
             visualize_pattern(stone_pattern["pattern"])
@@ -155,6 +175,26 @@ def simulate_cave(input_jet_stream, num_stones):
             if time_step == max_jet_stream_len:
                 time_step = 0
         stack_height = max(stack_height, cave.size - height_from_ceiling + 1)
+        if detect_repetition:
+            last_line_after_fall = cave[stack_height-1 if stack_height != 0 else 0]
+            if is_plane_ground and last_line_after_fall != 15 and last_line_after_fall != 120:
+                # possible candidate for repetition detection
+                if detect_repetition_time_list and (time_step in detect_repetition_time_list):
+                    match_idx = detect_repetition_time_list.index(time_step)
+                    base_stack_height = detect_repetition_height_list[match_idx]
+                    repetition_stack_height = stack_height_before_fall - base_stack_height
+                    base_stone_cnt = detect_repetition_stone_list[match_idx]
+                    repetition_stone_cnt = i-base_stone_cnt
+                    remainder_stones = (num_stones-base_stone_cnt)%repetition_stone_cnt
+                    repetition_cycles = (num_stones-base_stone_cnt)//repetition_stone_cnt
+                    remainder_height = simulate_cave(input_jet_stream, remainder_stones, detect_repetition=False, time_offset=time_step-4)
+                    stack_height = base_stack_height + repetition_cycles*repetition_stack_height + remainder_height
+                    break
+                else:
+                    detect_repetition_time_list.append(time_step)
+                    detect_repetition_height_list.append(stack_height_before_fall)
+                    detect_repetition_stone_list.append(i)
+
         if DEBUG:
             print("State of the cave:")
             visualize_pattern(cave)
@@ -164,9 +204,10 @@ def simulate_cave(input_jet_stream, num_stones):
 
 if __name__=="__main__":
     input_file = "17_1_input.txt"
-    input_file = "17_1_example_input.txt"
+    # input_file = "17_1_example_input.txt"
     input_jet_stream = parse_input(input_file)
     num_stones = 1000000000000
+    # num_stones = 10000000
     DEBUG = False
-    stack_height = simulate_cave(input_jet_stream, num_stones=num_stones)
+    stack_height = simulate_cave(input_jet_stream, num_stones=num_stones, detect_repetition=True)
     print("Height of the cave after {} stones is {}".format(num_stones, stack_height))
